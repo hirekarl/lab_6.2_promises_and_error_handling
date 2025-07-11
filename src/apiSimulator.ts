65313bf94a5501fc33f44878ff3f16524709b4f6
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-11 14:09:29 -0400

    Implemented fetchProductCatalog and related items.

--- src/apiSimulator.ts
+++ src/apiSimulator.ts
@@ -1,5 +1,31 @@
-function fetchProductCatalog() {
-  /* TODO */
+const FETCH_PRODUCT_DELAY: number = 1000
+
+type Product = {
+  id: number
+  name: string
+  price: number
+}
+
+const mockProducts: Product[] = [
+  { id: 0, name: "Fidget Spinner", price: 12.99 },
+  { id: 1, name: "Toy Car", price: 1.99 },
+  { id: 2, name: "Space Elevator", price: 4999999999.99 },
+]
+
+function isFetchProductCatalogSuccessful(): boolean {
+  return Math.random() < 0.5
+}
+
+function fetchProductCatalog(): Promise<Product[]> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isFetchProductCatalogSuccessful()) {
+        resolve(mockProducts)
+      } else {
+        reject("Failed to fetch product catalog.")
+      }
+    }, FETCH_PRODUCT_DELAY)
+  })
 }
 
 function fetchProductReview() {
