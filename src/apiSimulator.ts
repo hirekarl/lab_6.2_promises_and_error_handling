c56c8f63a314feb13bbacd68f06498ad7257faa0
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-07-11 13:57:07 -0400

    Added function skeleton to apiSimulator.ts.

---
+++ src/apiSimulator.ts
@@ -0,0 +1,13 @@
+function fetchProductCatalog() {
+  /* TODO */
+}
+
+function fetchProductReview() {
+  /* TODO */
+}
+
+function fetchSalesReport() {
+  /* TODO */
+}
+
+export { fetchProductCatalog, fetchProductReview, fetchSalesReport }
